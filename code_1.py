b0f4d47db0c6aba423d449c04efb578dbc2575d3
kolpaberit <143887775+kolpaberit@users.noreply.github.com>
2023-10-30 22:06:06 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,67 +1,27 @@
-        print(f"Random Number: {item}")
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-
-
-    main()
-    data = generate_random_data()
-def main():
 import random
-if __name__ == "__main__":
 
-def generate_random_data():
-    return data        int[] data = new int[10];
-}
-
-
-        }
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-    }
-            data[i] = (int) (Math.random() * 100) + 1;
-        for (int item : data) {
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-        }    for (let i = 0; i < 10; i++) {
-    const data = generateRandomData();
-function generateRandomData() {
-main();
-        data.push(Math.floor(Math.random() * 100) + 1);
-    data.forEach(item => {
-
-
-        console.log("Random Number: " + item);
-}
-    return data;
-function main() {
-}
-
-    const data = [];
-    });
-    }
-def generate_random_data():
-
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-def main():
-        print(f"Random Number: {item}")
-    return data
-    for item in data:
-if __name__ == "__main__":
-
-
-    data = generate_random_data()
 
     return data
-        print(f"Random Number: {item}")
+
+
 def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+if __name__ == "__main__":
+    data = generate_random_data()
+    for item in data:
+        print(f"Random Number: {item}")
 def main():
+def main():
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    return data
 
     for item in data:
-if __name__ == "__main__":
-    main()
+def generate_random_data():
 import random
+
+
     data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
+    main()
