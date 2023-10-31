476997fd13eab2af622804a76364329492d0ead5
kolpaberit <143887775+kolpaberit@users.noreply.github.com>
2023-10-31 23:16:21 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,26 @@
+        }
+
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+}
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+    return data
+if __name__ == "__main__":
+    main()
+def generate_random_data():
+
 
 import random
-    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
+    data = generate_random_data()
 def main():
 
-    main()
-    return data
         print(f"Random Number: {item}")
-
-if __name__ == "__main__":
-    for item in data:
-    data = generate_random_data()
-def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
