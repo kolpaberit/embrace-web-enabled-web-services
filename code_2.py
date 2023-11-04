a45a5d0ac89c994c6cccfd89fda32eee2efe91c5
kolpaberit <143887775+kolpaberit@users.noreply.github.com>
2023-11-04 02:42:26 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,66 +1,13 @@
-def main():
-    return data
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-
-
-        print(f"Random Number: {item}")
     data = generate_random_data()
-def generate_random_data():
-if __name__ == "__main__":
-    main()
     for item in data:
-import random
-def generate_random_data():
+def main():
 
     return data
-import random
 if __name__ == "__main__":
-        print(f"Random Number: {item}")
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
     main()
-    for item in data:
-
-    data = generate_random_data()    for item in data:
-if __name__ == "__main__":
     data = [random.randint(1, 100) for _ in range(10)]
-    return data
-    main()
-def main():
-import random
-def generate_random_data():
-
-
-        print(f"Random Number: {item}")
-
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-if __name__ == "__main__":
-
-def generate_random_data():
-import random
-
-    data = generate_random_data()
-def main():
-    return data
-    for item in data:
-if __name__ == "__main__":
-    data = generate_random_data()
-    return data
-import random
 
 
 def generate_random_data():
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-def main():
         print(f"Random Number: {item}")
+import random
